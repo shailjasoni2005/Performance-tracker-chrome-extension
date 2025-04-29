0bce1a0aed4fc7c4b4d9273e410fc765e7af615c
shailjasoni2005 <144000431+shailjasoni2005@users.noreply.github.com>
2025-04-29 15:40:09 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/chrome-extension/', // Ensures correct path resolution for index.html
+  base: './', // Ensures correct path resolution for index.html
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
